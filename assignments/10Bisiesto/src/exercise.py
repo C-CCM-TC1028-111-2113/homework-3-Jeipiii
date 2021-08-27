815966c882eb85a849df61e6fcaf69912303212b
Jeipiii <88851076+Jeipiii@users.noreply.github.com>
2021-08-27 14:50:15 -0500

    Update exercise.py

--- assignments/10Bisiesto/src/exercise.py
+++ assignments/10Bisiesto/src/exercise.py
@@ -1,6 +1,16 @@
 
 def main():
     #escribe tu código abajo de esta línea
+    año =int(input("Dame el valor de un año : "))
+    def es_bisiesto(año):
+        if año % 4 == 0:
+            True
+        elif año % 100 == 0:
+            False
+        else:
+            False
+
+    print(es_bisiesto(año)) 
     pass
 
 if __name__=='__main__':
