e4f4c6c5d1900be0e8ecaa865c786d4cd9cb278f
Jeipiii <88851076+Jeipiii@users.noreply.github.com>
2021-08-27 14:40:24 -0500

    Update exercise.py

--- assignments/08TarjeteriaEspanola/src/exercise.py
+++ assignments/08TarjeteriaEspanola/src/exercise.py
@@ -1,6 +1,15 @@
 
 def main():
     #escribe tu código abajo de esta línea
+    albanene = int(input("Dame el numero de pliegos de papel albanene : "))
+    plumones = int(input("Dame el numero de plumones : "))
+
+    def tarjetas():
+        alba = albanene * 12
+        plu = plumones * 35
+        tot_tar = (alba + plu)/35
+        return tot_tar
+    print(" El numero de tarjetas alcanza para :" + str(tarjetas()))
     pass
 
 if __name__=='__main__':
