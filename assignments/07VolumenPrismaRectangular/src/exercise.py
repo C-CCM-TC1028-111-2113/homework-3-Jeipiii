4939b4fa9a2d3477198c62b9df8c455387e9b640
Jeipiii <88851076+Jeipiii@users.noreply.github.com>
2021-08-27 14:30:20 -0500

    Update exercise.py
    base = int(input("Da un valor para la base : "))
    altura = int(input("Da un valor para la altura : "))
    profundidad = float(input("Da un valor para la profundidad" ))
               
    def area():
        area_1 = base * altura
        volumen = area_1 * profundidad
        return volumen
    print("El volumen del prisma es : "+ str(area()))

--- assignments/07VolumenPrismaRectangular/src/exercise.py
+++ assignments/07VolumenPrismaRectangular/src/exercise.py
@@ -1,6 +1,15 @@
 
 def main():
     #escribe tu código abajo de esta línea
+    base = int(input("Da un valor para la base : "))
+    altura = int(input("Da un valor para la altura : "))
+    profundidad = float(input("Da un valor para la profundidad" ))
+           
+    def area():
+        area_1 = base * altura
+        volumen = area_1 * profundidad
+        return volumen
+    print("El volumen del prisma es : "+ str(area()))
     pass
 
 if __name__=='__main__':
